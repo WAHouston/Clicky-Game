165c61c05ccdc5b77df572ba7c0de50cde199c75
Will H <wandrewhouston@gmail.com>
2019-01-15 17:21:30 -0500

    Updates

---
+++ precache-manifest.50d58943cbcf4e83e5967d7fd0953fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "ff390e827abba080faba",
+    "url": "/Clicky-Game/static/js/main.ff390e82.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ff390e827abba080faba",
+    "url": "/Clicky-Game/static/css/main.468e2498.chunk.css"
+  },
+  {
+    "revision": "8bb03b0008496d490fb467c48934d7f5",
+    "url": "/Clicky-Game/index.html"
+  }
+];
